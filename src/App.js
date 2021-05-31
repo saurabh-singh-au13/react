1053f6f8f4a134286e1b7eb0e6c3b4ef31fe5b0c
saurabh <saurabh@gmail.com>
2021-05-31 21:46:43 +0530

    Use_Google_Font_React

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import './App.css';
 import React from "react"
 
 function App() {
-  const img = "https://picsum.photos/200/300"
-  const img1 = "https://picsum.photos/300/300"
-  const img2 = "https://picsum.photos/300/250"
-  const img3 = "https://picsum.photos/400/300"
-  return (
+  return(
+
     <div className="App">
       <>
-      <h1 className="xyz" contentEditable='true' spellCheck="true"> What is your name ---</h1>
-      <img src={img} alt="kuch"/>
-      <a href="https://google.com/" target="_xyz"><img src={img1} alt="kuch"/> </a>
-      
-      <img src={img2} alt="kuch1"/>
-      <img src={img3} alt="kuch"/>
+      <h1> My name is Saurabh Kumar Singh </h1>
+      <h1> My name is Saurabh Kumar Singh lorem10
+         </h1>
       </>
     </div>
   );
