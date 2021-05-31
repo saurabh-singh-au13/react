57e80d0511c8f1c4e34c64b45cd358ea2ea1382d
saurabh <saurabh@gmail.com>
2021-05-31 13:32:14 +0530

    Basic_React_Jsx_Attributes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import React from "react"
 
 function App() {
-  const curDate = new Date().toLocaleString()
-  const curTime = new Date().toLocaleTimeString()
-  
-
+  const img = "https://picsum.photos/200/300"
+  const img1 = "https://picsum.photos/300/300"
+  const img2 = "https://picsum.photos/300/250"
+  const img3 = "https://picsum.photos/400/300"
   return (
     <div className="App">
       <>
-        <h1>Display Today Date and Time</h1> 
-        <h3> Today Date is {`${curDate}`}</h3>
-        <h3> Today Time is {`${curTime}`}</h3>
+      <h1 contentEditable='true' spellCheck="true"> What is your name ---</h1>
+      <img src={img} alt="kuch"/>
+      <a href="https://google.com/" target="_xyz"><img src={img1} alt="kuch"/> </a>
+      
+      <img src={img2} alt="kuch1"/>
+      <img src={img3} alt="kuch"/>
       </>
     </div>
   );
