505db834cf09754811e6f60f060408d8d37702ab
saurabh <saurabh@gmail.com>
2021-05-31 11:23:06 +0530

    JSX_Expression_in_React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import React from "react"
 
-
 function App() {
+  const myTeam = "Alpha"
+  const myAge = 23
+  const Sum = "5"
   return (
     <div className="App">
-      <React.Fragment>
+      <>
         <h1> Saurabh </h1>
-      </React.Fragment>
+        
+        <h2> I love to work with my {myTeam}</h2>
+        <p> And also my age is {myAge} </p>
+        <span> what is 2+3 </span>
+        <span> The sum of 2 and 3 is {Sum} </span>
+        <p> sum of 4 and 5 is {4+5}</p>
+      </>
       
+
      
     </div>
   );
