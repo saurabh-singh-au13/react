4de5222f044151433c1584463de762c4e6d406d7
saurabh <saurabh@gmail.com>
2021-05-31 22:07:48 +0530

    Inline_External_Internal_Css_inReact

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import './App.css';
 import React from "react"
 
 function App() {
+  const myStyle = {
+    color: "green",
+    fontSize:"50px"
+
+  }
+  const myStyle1 = {
+    color: "tomato",
+    fontSize: "50px"
+
+  }
   return(
+    
 
     <div className="App">
       <>
-      <h1> My name is Saurabh Kumar Singh </h1>
-      <h1> My name is Saurabh Kumar Singh lorem10
-         </h1>
+      <h1 style={{color: "red", background:"black"}}> My name is Saurabh Kumar Singh </h1>
+      <h1 style={myStyle}> My name is Saurabh Kumar Singh lorem10
+      </h1>
+      <p contentEditable="true" style = {myStyle1}> Note: In JSX, JavaScript expressions are written inside curly braces, and since JavaScript objects also use curly braces, the styling in the example above is written inside two sets of curly braces.</p>
       </>
     </div>
   );
